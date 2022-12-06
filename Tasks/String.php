9a9de6b206e7960ad8fb1f7c7b1015d274147c19
mohammedmakhlouf78 <mohammedmakhlouf78@gmail.com>
2022-12-06 16:30:07 +0200

    sub string and strpos

---
+++ Tasks/String.php
@@ -0,0 +1,67 @@
+<?php
+
+/*********************** */
+/* Task 1 */
+/*********************** */
+$name = "mohammedmakhlouf78";
+
+//get from the middle to the end
+
+/*********************** */
+/* Task 1 */
+/*********************** */
+
+
+/*********************** */
+/* Task 2 */
+/*********************** */
+$name = "mohammedmakhlouf78";
+
+//get from the start to the middle
+
+/*********************** */
+/* Task 2 */
+/*********************** */
+
+
+
+
+/*********************** */
+/* Task 3 */
+/*********************** */
+$name = "mohammedmakhlouf78";
+
+//get the last 5 letters
+
+/*********************** */
+/* Task 3 */
+/*********************** */
+
+
+
+
+
+
+
+/*********************** */
+/* Task 4 */
+/*********************** */
+$dsec = "hello my name is mohammed and i love cats and my email is alimakhlouf@gmail.com";
+
+// get the last 20 letters of the string and check if they are a valid email
+/*********************** */
+/* Task 4 */
+/*********************** */
+
+
+/*********************** */
+/* Task 5 */
+/*********************** */
+
+$name = "mohammedmakhlouf78";
+
+// get 3 letters from the middle
+
+/*********************** */
+/* Task 5 */
+/*********************** */
