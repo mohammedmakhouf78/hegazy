b31b2b705c0eae0d493a92dacabedd6cf755791c
abdoallahegazy <hegazyh497@gmail.com>
2022-12-07 20:12:16 +0200

    is and tesks

--- Hegazy/String/string.php
+++ Hegazy/String/string.php
@@ -1,3 +1,84 @@
 <?php
 
-echo "hegazy";
+// echo "hegazy"
+
+
+
+/*********************** */
+/* Task 1 */
+/*********************** */
+$name = "mohammedmakhlouf78";
+$strle = strlen($name) / 2;
+// echo substr($name, $strle);
+echo "\n";
+//get from the middle to the end
+
+/*********************** */
+/* Task 1 */
+/*********************** */
+
+
+
+/*********************** */
+/* Task 2 */
+/*********************** */
+$name = "mohammedmakhlouf78";
+$strle = strlen($name) / 2;
+// echo substr($name, 0, -$strle);
+echo "\n";
+//get from the start to the middle
+
+/*********************** */
+/* Task 2 */
+/*********************** */
+
+
+
+
+
+/*********************** */
+/* Task 3 */
+/*********************** */
+$name = "mohammedmakhlouf78";
+// echo substr($name, -5);
+echo "\n";
+//get the last 5 letters
+
+/*********************** */
+/* Task 3 */
+/*********************** */
+
+
+
+/*********************** */
+/* Task 4 */
+/*********************** */
+$dsec = "hello my name is mohammed and i love cats and my email is alimakhlouf@gmail.com";
+$substr = substr($dsec, -20);
+$strpos = strpos($substr, '@');
+// if (strpos($substr, '@') === false) {
+//     echo "No exist @ \n";
+// } else {
+//     echo "Yes exist @ \n";
+// }
+echo "\n";
+// get the last 20 letters of the string and check if they are a valid email
+/*********************** */
+/* Task 4 */
+/*********************** */
+
+
+
+/*********************** */
+/* Task 5 */
+/*********************** */
+
+$name = "mohammedmakhlouf78";
+$strle = strlen($name) / 2;
+echo substr($name, $strle, 3);
+echo "\n";
+// get 3 letters from the middle
+
+/*********************** */
+/* Task 5 */
+/*********************** */
