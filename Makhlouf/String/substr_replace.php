f02fbed34a70b060862a7539f88381737dfd408c
mohammedmakhlouf78 <mohammedmakhlouf78@gmail.com>
2022-12-08 21:28:31 +0200

    str replace

---
+++ Makhlouf/String/substr_replace.php
@@ -0,0 +1,69 @@
+<?php
+
+
+$name = "my name is mohammed sharaf iam a good person and i like to eat chikens grill";
+
+echo strrev($name);
+
+echo "\n";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $str = "a my name is mohammed sharaf iam a good person and i like to eat chikens grill";
+
+
+// $count = 0;
+
+// for($i = 0; $i < strlen($str); $i++)
+// {
+
+//     if(strstr("aeiouAEIOU", $str[$i]))
+//     {
+//         $count++;
+//     }
+
+// }
+
+
+// echo $count;
+// echo "\n";
+
+
+
+
+
+
+
+
+
+
+// $name = "doh fuck adslk jalkds jflakdsj flkjdsa lfj";
+
+// echo substr_replace($name, "...",15);
+
+// echo "\n";
+
---
+++ Tasks/String/substr_replace.php
@@ -0,0 +1,39 @@
+<?php
+
+
+//using that string ellipsis(...) it from the middle
+
+$str = "my name is mohammed sharaf iam a good person and i like to eat chikens grill";
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using that string count how many (a) and how many (o) and how many (m)
+$str = "my name is mohammed sharaf iam a good person and i like to eat chikens grill";
+
+
+
+
+
+
+//using that string reverse the string letters (using loops)
+$str = "my name is mohammed sharaf iam a good person and i like to eat chikens grill";
+
+
+
+
+
+//using that string reverse the string words
+$str = "my name is mohammed sharaf iam a good person and i like to eat chikens grill";
+
+
+
