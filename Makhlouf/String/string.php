8ed7c89dc0208e4ce0356478d56d5f58e6de2721
mohammedmakhlouf78 <mohammedmakhlouf78@gmail.com>
2022-12-19 13:44:43 +0200

    updates

--- Makhlouf/String/string.php
+++ Makhlouf/String/string.php
@@ -1,84 +1,12 @@
 <?php
 
+$email = "@aligmail.com";
 
-
-
-
-//substring
-
-$name = "mohammedmakhlouf78";
-
-echo substr($name, 5, -3);
-
-echo "\n";
-echo "\n";
-echo "\n";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// strpos
-
-$email = '@aligmail.com';
-
-if(strpos($email, '@') === false)
+if(strpos($email, "@") === false)
 {
-    echo "the email is Wrong \n";
+    echo "there is an error \n";
+    die;
 }
-else
-{
-    echo "The email is Ok \n";
-}
-echo "\n";
-// strpos
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//strpos
-
-//now doc
-$name = "ahmed";
-
-$x = <<<'__END__'
-    hello ahmed
-    $name
-__END__;
-
-echo $x;
-//now doc
+echo "success\n";
 
 echo "\n";
