22cbaa0431cd111003ad7162155bb2a7ce03e77f
mohammedmakhlouf78 <mohammedmakhlouf78@gmail.com>
2022-12-27 13:52:41 +0200

    updates

---
+++ Makhlouf/String/csv.php
@@ -0,0 +1,108 @@
+<?php
+
+
+//this is for buffering 
+
+// $sales = [
+//     ['Northeast','2005-01-01','2005-02-01',12.54],
+//     ['Northwest','2005-01-01','2005-02-01',546.33],
+//     ['Southeast','2005-01-01','2005-02-01',93.26],
+//     ['Southwest','2005-01-01','2005-02-01',945.21],
+//     ['All Regions','--','--',1597.34],
+// ];
+
+
+// $file = fopen("php://output", '');
+
+// ob_start();
+// foreach($sales as $sale)
+// {
+//     fputcsv($file,$sale);
+// }
+
+// $x = ob_get_contents();
+
+// ob_end_clean();
+
+// print_r($x);
+
+// fclose($file);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//for printing only
+
+// $sales = [
+//     ['Northeast','2005-01-01','2005-02-01',12.54],
+//     ['Northwest','2005-01-01','2005-02-01',546.33],
+//     ['Southeast','2005-01-01','2005-02-01',93.26],
+//     ['Southwest','2005-01-01','2005-02-01',945.21],
+//     ['All Regions','--','--',1597.34],
+// ];
+
+// $file = fopen("php://output", '');
+
+// foreach($sales as $sale)
+// {
+//     fputcsv($file,$sale);
+// }
+
+// fclose($file);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $str = "ahmed,10,male,500$,cairo"; // csv coma separated values
+
+// $sales = [
+//     ['Northeast','2005-01-01','2005-02-01',12.54],
+//     ['Northwest','2005-01-01','2005-02-01',546.33],
+//     ['Southeast','2005-01-01','2005-02-01',93.26],
+//     ['Southwest','2005-01-01','2005-02-01',945.21],
+//     ['All Regions','--','--',1597.34],
+// ];
+
+// $file = fopen('./sales.csv','w+');
+
+// foreach($sales as $sale)
+// {
+//     fputcsv($file,$sale);
+// }
+
+// fclose($file);
