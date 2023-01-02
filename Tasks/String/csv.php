4eeccfabd363f624ad0dab827df2d8573f9a9060
M0hamedSharaf <sazxsazx987987@gmail.com>
2023-01-02 18:07:55 +0200

    display the sales.csv file in an html table

--- Tasks/String/csv.php
+++ Tasks/String/csv.php
@@ -4,3 +4,42 @@
  * display the sales.csv file in an html table
  * using the method fgetcsv
  */
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>+test</title>
+    <style>
+    table,
+    th,
+    td {
+        border: 1px solid;
+    }
+    </style>
+
+</head>
+
+<body>
+    <?php
+    if (($file = fopen("sales.csv", "r")) !== false) {
+        $str = '';
+        $str .= '<table>';
+        while (($resuletData = fgetcsv($file, 1024, ",")) !== false) {
+            $str .= '<tr>';
+            foreach ($resuletData as $key => $value) {
+                $str .= "<td>$value</td>";
+            }
+            $str .= '</tr>';
+        }
+        fclose($file);
+        $str .= '</table>';
+        echo $str;
+    }
+    ?>
+</body>
+
+</html>
+
+<!-- &$value -->
