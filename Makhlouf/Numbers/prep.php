a344425be986dd59efed0e11e859e001d6154763
mohammedmakhlouf78 <mohammedmakhlouf78@gmail.com>
2023-01-19 02:06:05 +0200

    updates

--- Makhlouf/Numbers/prep.php
+++ Makhlouf/Numbers/prep.php
@@ -1,221 +1,31 @@
 <?php
 
+$ads = array('ford' => 12234,
+'att' => 33424,
+'ibm' => 16823);
 
-
-function printBetween(int|string $start,int|string $end)
+function rand_weighted($numbers)
 {
-    $numbers = myRange($start, $end);
-
-    foreach($numbers as $key => $number)
+    $total = 0;
+    $distributions = [];
+    foreach($numbers as $key => $weight)
     {
-        printf("the number is %s => %s \n", $key, $number);
+        $total += $weight;
+        $distributions[$key] = $total;
+    }
+
+    $rand = rand(0, $total - 1);
+
+    foreach($distributions as $key => $weight)
+    {
+        if($rand < $weight)
+        {
+            return $key;
+        }
     }
 }
 
-
-printBetween(1, 1000000);
-
-function myRange($start, $end)
-{
-    for($i = $start; $i <= $end; $i++)
-    {
-        yield "ahmed $i" => $i;
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Collection extends ArrayIterator
-// {
-
-// }
-
-
-
-// $collection = new Collection([
-//     "ahmed",
-//     "ali",
-//     "omar",
-//     'hassan',
-//     "hussien"
-// ]);
-
-
-// foreach($collection as $key => $value)
-// {
-//     echo "key => $key    item => $value \n"; 
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Collection implements Iterator
-// {
-//     public $index = 0;
-
-//     private $data = [
-//         "ahmed",
-//         "ali",
-//         "mohammed",
-//         "yussof",
-//         'samir'
-//     ];
-//     public $x2 = 15;
-//     public $x3 = 50;
-//     public $x4 = 3;
-//     public $x5 = 7;
-
-//     protected $y = 13;
-//     private $z = 55;
-
-//     public function __construct()
-//     {
-        
-//     }
-
-//     public function current(): mixed
-//     {
-//         return $this->data[ $this->index ];
-//     }
-
-//     public function key(): mixed
-//     {
-//         return $this->index;
-//     }
-
-//     public function next(): void
-//     {
-//         $this->index++;
-//     }
-
-//     public function rewind(): void
-//     {
-//         $this->index = 0;
-//     }
-
-//     public function valid(): bool
-//     {
-//         return isset( $this->data[ $this->index ] );
-//     }
-
-// }
-
-
-// $collection = new Collection();
-
-// foreach($collection as $key => $item)
-// {
-//     echo "key => $key    item => $item \n"; 
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function printBetween(int|string $start,int|string $end)
-// {
-//     $numbers = range($start, $end);
-
-//     foreach($numbers as $number)
-//     {
-//         printf("the number is %s \n", $number);
-//     }
-// }
-
-
-// // printBetween(' ', 't');
-
+echo rand_weighted($ads) . "\n";
+echo rand_weighted($ads) . "\n";
+echo rand_weighted($ads) . "\n";
+echo rand_weighted($ads) . "\n";
---
+++ Makhlouf/Numbers/random.php
@@ -0,0 +1,15 @@
+<?php
+/*
+
+https://stackoverflow.com/questions/445235/generating-random-results-by-weight-in-php
+
+https://www.youtube.com/watch?v=_tN2ev3hO14
+
+*/
+
+
+echo rand(1,100) . "\n"; // mersenne twister
+echo mt_rand(1,100) . "\n"; // mersenne twister
+echo mt_rand(1,100) . "\n";
+echo mt_rand(1,100) . "\n";
+echo mt_rand(1,100) . "\n";
