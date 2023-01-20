3d146d21dcd8626baa3079ed31571bd1e1359f73
mohammedmakhlouf78 <mohammedmakhlouf78@gmail.com>
2023-01-20 03:35:15 +0200

    updates

---
+++ Makhlouf/Numbers/logarithms.php
@@ -0,0 +1,71 @@
+<?php
+
+
+
+// echo pow(-2, 0.5) . "\n";
+// echo pow(2, M_E) . "\n";
+// echo pow(16, 0.5) . "\n";
+// echo pow(8, -2) . "\n";
+// echo pow(2, 3) . "\n";
+// echo pow(2, 3) . "\n";
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(is_infinite(pow(2, 100000000)))
+// {
+//     echo "hi \n";
+// }
+
+
+
+
+
+
+
+// echo exp(10);
+
+
+
+
+
+// echo log(-10) . "\n";
+// echo gettype(NAN) . "\n";
+// if(typeof log(-10))
+// {
+//     echo "hi \n";
+// }
+
+// $log = log10(10);
+
+// echo $log . "\n";
+
+
+
+
+
+
+
+
+
+// $log = log(10); // the base here is (e) بتاعت الواد اويلر
+
+// echo $log . "\n";
+
+
+
+
+
+
+// $log = log(8, 2); // اس 2 يطلع كام
+
+// echo $log . "\n";
--- Makhlouf/Numbers/random.php
+++ Makhlouf/Numbers/random.php
@@ -7,9 +7,119 @@ https://www.youtube.com/watch?v=_tN2ev3hO14
 
 */
 
+function getRandomWeightedElement(array $weightedValues) 
+{
+    $rand = mt_rand(1, (int) array_sum($weightedValues)); 
+    
+    foreach ($weightedValues as $key => $value) 
+    {
+      $rand -= $value; 
+      if ($rand <= 0) {
+        return $key;
+      }
+    }
+}
 
-echo rand(1,100) . "\n"; // mersenne twister
-echo mt_rand(1,100) . "\n"; // mersenne twister
-echo mt_rand(1,100) . "\n";
-echo mt_rand(1,100) . "\n";
-echo mt_rand(1,100) . "\n";
+
+
+
+$data = [
+    'toyota' => 100000,
+    'mercedes' => 300000,
+    'BMW' => 50000,
+    'Hundai' => 10000,
+    'caro' => 300,
+];
+
+
+$counters = [
+    'toyota' => 0,
+    'mercedes' => 0,
+    'BMW' => 0,
+    'Hundai' => 0,
+    'caro' => 0,
+];
+for($i = 1; $i <= 1000; $i++)
+{
+    $car = getRandomWeightedElement($data);
+    $counters[$car] += 1;
+}
+
+print_r($counters);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function randomBiased()
+// {
+//     $x = rand(0, 100);
+
+//     if($x <= 10)
+//     {
+//         return rand(1,5);
+//     }
+//     else
+//     {
+//         return rand(6,10);
+//     }
+// }
+
+// $counter = 0;
+// for($i = 1; $i <= 1000; $i++)
+// {
+//     if(randomBiased() > 5)
+//     {
+//         $counter++;
+//     }
+// }
+
+// echo "above 5 " . $counter . "\n";
+// echo "below 5 " . (1000 - $counter) . "\n";
+
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+// echo randomBiased() . "\n";
+
+// echo rand(1,100) . "\n"; // mersenne twister
+// echo mt_rand(1,100) . "\n"; // mersenne twister
+// echo mt_rand(1,100) . "\n";
+// echo mt_rand(1,100) . "\n";
+// echo mt_rand(1,100) . "\n";
