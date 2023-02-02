1df8b3827382206306302afef9d54bb6da15fdea
mohammedmakhlouf78 <mohammedmakhlouf78@gmail.com>
2023-02-02 17:12:01 +0200

    updates

---
+++ Makhlouf/DateTime/intro.php
@@ -0,0 +1,50 @@
+<?php
+
+$date = mktime(13,51,0,2,2,2023);
+
+// https://www.php.net/manual/en/datetime.format.php
+date_default_timezone_set('Australia/Adelaide');
+
+// echo date("Y-m-d h:i H:i", $date) . "\n";
+
+echo "d ==> " . date('d') . "\n";
+echo "D ==> " . date('D') . "\n";
+echo "j ==> " . date('j') . "\n";
+echo "l ==> " . date('l') . "\n";
+echo "L ==> " . date('L') . "\n";
+echo "N ==> " . date('N') . "\n";
+echo "S ==> " . date('S') . "\n";
+echo "w ==> " . date('w') . "\n";
+echo "z ==> " . date('z') . "\n";
+echo "W ==> " . date('W') . "\n";
+echo "F ==> " . date('F') . "\n";
+echo "m ==> " . date('m') . "\n";
+echo "M ==> " . date('M') . "\n";
+echo "t ==> " . date('t') . "\n";
+echo "L ==> " . date('L') . "\n";
+echo "o ==> " . date('o') . "\n";
+echo "X ==> " . date('X') . "\n";
+echo "x ==> " . date('x') . "\n";
+echo "Y ==> " . date('Y') . "\n";
+echo "y ==> " . date('y') . "\n";
+echo "a ==> " . date('a') . "\n";
+echo "A ==> " . date('A') . "\n";
+echo "B ==> " . date('B') . "\n";
+echo "g ==> " . date('g') . "\n";
+echo "G ==> " . date('G') . "\n";
+echo "h ==> " . date('h') . "\n";
+echo "H ==> " . date('H') . "\n";
+echo "i ==> " . date('i') . "\n";
+echo "s ==> " . date('s') . "\n";
+echo "u ==> " . date('u') . "\n";
+echo "v ==> " . date('v') . "\n";
+echo "e ==> " . date('e') . "\n";
+echo "I ==> " . date('I') . "\n";
+echo "o ==> " . date('o') . "\n";
+echo "P ==> " . date('P') . "\n";
+echo "p ==> " . date('p') . "\n";
+echo "T ==> " . date('T') . "\n";
+echo "Z ==> " . date('Z') . "\n";
+echo "c ==> " . date('c') . "\n";
+echo "r ==> " . date('r') . "\n";
+echo "U ==> " . date('U') . "\n";
