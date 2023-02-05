ba29c584450456ec33e36237f7b9936fde8a4c82
mohammedmakhlouf78 <mohammedmakhlouf78@gmail.com>
2023-02-05 08:10:26 +0200

    updates

---
+++ Makhlouf/arrays/intro.php
@@ -0,0 +1,263 @@
+<?php
+
+echo "<h1>" . implode("<h1> <h1>",['ahmed','ali','ebrahim','adfdsa']) . "<h1>" . "\n";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $fruits1 = ['banana','x'  =>  'red' , 'apple', 'sharaf'];
+// $fruits2 = ['wow', 'red', 'x' => 'ahmed'];
+
+
+// $x = array_merge($fruits1, $fruits2);
+
+// print_r($x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $fruits = ['ali', 'mohammed', 'ahmed', 'ebrahim'];
+
+// print_r($fruits);
+
+// $result = array_splice($fruits, 1,2);
+
+// print_r($result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $fruits = ['ali', 'mohammed', 'ahmed', 'ebrahim'];
+
+// print_r($fruits);
+
+// array_shift($fruits);
+// array_shift($fruits);
+
+// print_r($fruits);
+
+
+
+
+
+
+
+
+
+
+
+// $fruits = ['ali', 'mohammed', 'ahmed', 'ebrahim'];
+
+// print_r($fruits);
+
+// unset($fruits[2]);
+
+// print_r($fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $a = array("2", "4", "6", "a");
+// echo "product(a) = " . array_product($a) . "\n";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $fruits = [
+//     'names' => [
+//         'ahmed',
+//         'ali',
+//         'ebrahim'
+//     ],
+//     'ages' => [
+//         '20',
+//         '15',
+//         '30'
+//     ]
+// ];
+
+// foreach($fruits as $key => $value)
+// {
+//     foreach($value as $item)
+//     {
+//         echo $item . "\n";
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $fruits ['white'][] = "sharaf";
+// $fruits ['white'][] = "betengan";
+
+// print_r($fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $fruits = [ 
+//     1 => "apple" ,
+//     'ahmed',
+//     "x" => "ali",
+//     "ebrahim"
+// ];
+
+// print_r($fruits);
+
+
+
+
+
+
+
+
+
+
+
+// $fruits = [ 
+//     "first" => "apple" ,
+//     'second' => 'ahmed'
+// ];
+
+// ["first" => $x1,"second" => $x2] = $fruits;
+
+
+// echo $x1 . "\n";
+// echo $x2 . "\n";
+
+
+
+
+
+
+// $fruits = [ 
+//     "first" => "apple" ,
+//     'second' => 'ahmed'
+// ];
+
+// print_r($fruits);
+
+// $fruits ["second"]= "banana";
+
+// print_r($fruits);
+
+
+
+// $fruits = [ "x" => "apple" ];
+
+// print_r($fruits);
+
+// $fruits []= "banana";
+
+// print_r($fruits);
